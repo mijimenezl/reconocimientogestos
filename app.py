24f581e9123732808307edef3f643deee9afc9cd
mijimenezl <167145289+mijimenezl@users.noreply.github.com>
2024-10-02 09:53:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 st.title("Reconocimiento de gestos!")
 #st.write("Versión de Python:", platform.python_version())
-image = Image.open('OIG5.jpg')
+image = Image.open('reconocimiento.jpg')
 st.image(image, width=500)
 with st.sidebar:
     st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
