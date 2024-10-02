310ce066d4fd92bca52ba5a801f414013d8bf2e5
mijimenezl <167145289+mijimenezl@users.noreply.github.com>
2024-10-02 09:46:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ st.write("Versión de Python:", platform.python_version())
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-st.title("Reconocimiento de Imágenes")
+st.title("Reconocimiento de gestos!")
 #st.write("Versión de Python:", platform.python_version())
-image = Image.open('OIG5.jpg')
-st.image(image, width=350)
+image = Image.open('reconocimiento.jpg')
+st.image(image, width=500)
 with st.sidebar:
     st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
 img_file_buffer = st.camera_input("Toma una Foto")
